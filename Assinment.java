b08c61688122f3744a7e9a9729b9606a3bb46d9b
Ayush singh <ayush.singh281022@gmail.com>
2026-05-10 11:28:39 +0530

    Add files via upload

---
+++ Assinment.java
@@ -0,0 +1,8 @@
+public class Assinment {
+public static void main(String[] args) {
+    int myInt = 21;
+    System.out.println(myInt);
+    int yourInt = myInt;
+    System.out.println(yourInt);
+}
+}
